0a7f3a88c19564e3ebce00c0fd86527b4133e9e9
ChernyshovNikita <workxrom@gmail.com>
2022-02-19 12:15:59 +0300

    feat: education info + hobbies info

--- src/containers/EducationAndHobby/EducationAndHobby.tsx
+++ src/containers/EducationAndHobby/EducationAndHobby.tsx
@@ -1,19 +1,74 @@
+import {css} from 'styled-components';
 import {Container, Text, Image, ImageBox} from '../../components';
 import {blockMarginCss} from '../../components/Text';
+import cx from 'classnames';
+
 import me from './me.jpg';
+import hexapod from './hexapod.png';
+import snowboarding from './snowboarding.jpeg';
 
 export type EducationAndHobbyProps = {};
 
 export const EducationAndHobby = ({}: EducationAndHobbyProps) => {
   return (
-    <Container>
-      <Text variant="h3" className={blockMarginCss}>
-        Education and hobbies:
-      </Text>
+    <div>
+      <Container>
+        <Text variant="h3" className={blockMarginCss}>
+          Education:
+        </Text>
 
-      <ImageBox>
-        <Image src={me} />
-      </ImageBox>
-    </Container>
+        <Text variant="h3">---</Text>
+        <Text variant="h3">
+          Bauman Moscow State Technical University (Russia):
+        </Text>
+        <Text variant="h3">
+          School of Robotic Technologies and Complex Automatization
+        </Text>
+        <Text variant="h3">---</Text>
+        <Text variant="h3">Glyndwˆr University (UK):</Text>
+        <Text variant="h3">Computer Science, Master’s degree</Text>
+        <Text variant="h3" className={blockMarginCss}>
+          ---
+        </Text>
+
+        <ImageBox className={cx(photoCss)}>
+          <Image src={me} />
+        </ImageBox>
+      </Container>
+      <Container>
+        <Text variant="h3" className={blockMarginCss}>
+          Hobbies:
+        </Text>
+
+        <Text variant="h3">---</Text>
+        <Text variant="h3">
+          Robots: hexapod, balance robot (arduino, raspberry pi)
+        </Text>
+        <Text variant="h3">---</Text>
+        <ImageBox className={cx(photoFullWidthCss)}>
+          <Image src={hexapod} />
+        </ImageBox>
+
+        <Text variant="h3">---</Text>
+        <Text variant="h3">Snowboarding</Text>
+        <Text variant="h3">---</Text>
+        <ImageBox className={cx(photoCss)}>
+          <Image src={snowboarding} />
+        </ImageBox>
+      </Container>
+    </div>
   );
 };
+
+const photoFullWidthCss = css`
+  min-height: 40vw;
+  max-height: 60vw;
+  min-width: 100%;
+  max-width: 100%;
+`;
+const photoCss = css`
+  min-height: 60vw;
+  max-height: 60vw;
+  min-width: 60vw;
+  max-width: 60vw;
+`;
