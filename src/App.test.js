ff67c8b203bdc411d2dbdc7346c184d09bf98bfc
ChernyshovNikita <workxrom@gmail.com>
2022-03-01 05:44:52 +0300

    feat: test circleCI

--- src/App.test.js
+++ src/App.test.js
@@ -5,13 +5,17 @@ const name = 'Chernyshov Nikita';
 
 test(`render full page with text ${name}`, () => {
   render(<App />);
+
   const names = screen.getAllByText(name);
+
   expect(names.length).toBe(2);
 });
 
 test(`render img`, () => {
   render(<App />);
+
   // find img by aria-label='me'
   const img = screen.getByRole('img', {name: 'me'});
+
   expect(img).toBeInTheDocument();
 });
