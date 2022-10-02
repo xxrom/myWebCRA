b49ad72b6054f992e507b35b4238d00645338e18
ChernyshovNikita <workxrom@gmail.com>
2022-10-02 16:44:53 +0400

    feat: working webpack-dev-server reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,55 @@
 //import logo from './logo.svg';
 //import './App.css';
-import {Main, TodoReduxPage, TodoUseReducerPage, UI} from './pages';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Layout} from './containers';
-import {createGlobalStyle} from 'styled-components';
-import {Test0} from './test/';
+export const App = () => {};
+//import {Main, TodoReduxPage, TodoUseReducerPage, UI} from './pages';
+//import {BrowserRouter, Routes, Route} from 'react-router-dom';
+//import {Layout} from './containers';
+//import {createGlobalStyle} from 'styled-components';
+//import {Test0} from './test/';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      {/* A <Routes> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="new" element={<NewPage />} />
-          <Route path="ui" element={<UI />} />
+//const App = () => {
+//return (
+//<BrowserRouter>
+//<GlobalStyle />
+//[> A <Routes> looks through its children <Route>s and
+//renders the first one that matches the current URL. */}
+//<Routes>
+//<Route path="/" element={<Layout />}>
+//<Route index element={<Main />} />
+//<Route path="new" element={<NewPage />} />
+//<Route path="ui" element={<UI />} />
 
-          <Route path="test0" element={<Test0 />} />
-        </Route>
-        <Route path="/todo-use-reducer" element={<Layout />}>
-          <Route index element={<TodoUseReducerPage />} />
-        </Route>
-        <Route path="/todo-redux" element={<Layout />}>
-          <Route index element={<TodoReduxPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+//<Route path="test0" element={<Test0 />} />
+//</Route>
+//<Route path="/todo-use-reducer" element={<Layout />}>
+//<Route index element={<TodoUseReducerPage />} />
+//</Route>
+//<Route path="/todo-redux" element={<Layout />}>
+//<Route index element={<TodoReduxPage />} />
+//</Route>
+//</Routes>
+//</BrowserRouter>
+//);
+//};
 
-export default App;
+//export default App;
 
-const NewPage = () => {
-  return (
-    <div>
-      <h1>new</h1>
-    </div>
-  );
-};
+//const NewPage = () => {
+//return (
+//<div>
+//<h1>new</h1>
+//</div>
+//);
+//};
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
+//const GlobalStyle = createGlobalStyle`
+//* {
+//box-sizing: border-box;
+//}
 
-  body {
-    position: relative;
-    margin: 0;
-    background: #efefef0f;
-  }
-`;
+//body {
+//position: relative;
+//margin: 0;
+//background: #efefef0f;
+//}
+//`;
---
+++ src/Test.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Test = ({ test }: { test?: string }) => (
+  <div>{`Test 44554 ${test}`}</div>
+);
