6005fca8d1a56823578e2de46f80614ae128cd3d
ChernyshovNikita <workxrom@gmail.com>
2022-10-07 04:26:56 +0400

    feat: revealed Main page

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { useBabelRc, override, addWebpackModuleRule } = require("customize-cra");
+const { useBabelRc, override, addWebpackModuleRule } = require('customize-cra');
 
 module.exports = override(
   useBabelRc(),
@@ -6,14 +6,14 @@ module.exports = override(
     test: /\.(tsx|ts)$/,
     exclude: /node_modules/,
     use: [
-      { loader: "babel-loader" },
+      { loader: 'babel-loader' },
       {
-        loader: "@linaria/webpack-loader",
+        loader: '@linaria/webpack-loader',
         options: {
-          sourceMap: process.env.NODE_ENV !== "production",
-          cacheDirectory: "cache/linaria",
-          extension: ".linaria.css",
-          preprocessor: "none",
+          sourceMap: process.env.NODE_ENV !== 'production',
+          cacheDirectory: 'cache/linaria',
+          extension: '.linaria.css',
+          preprocessor: 'none',
         },
       },
     ],
