6427be271988fa46b58e624771e693cbf009080a
ChernyshovNikita <workxrom@gmail.com>
2022-10-08 14:40:42 +0400

    feat: package updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-// import './index.css';
 import './global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
@@ -13,27 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-/*
-import ReactDOMClient from "react-dom/client";
-import App from "./App";
-
-const element = document.getElementById("root");
-const root = element && ReactDOMClient.createRoot(element);
-root?.render(<App />);
-
-if (module.hot) {
-  module.hot.accept("./App", () => {
-    const UpdatedApp = require("./App").default;
-    const element = document.getElementById("root");
-    const root = element && ReactDOMClient.createRoot(element);
-
-    root?.render(<UpdatedApp />);
-  });
-}
-*/
