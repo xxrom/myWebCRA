16e4f22786eba66ef87dccbf50dfb1f2dc70de79
ChernyshovNikita <workxrom@gmail.com>
2023-04-12 01:59:30 +0400

    feat: add background gradient

--- src/containers/Spline.tsx
+++ src/containers/Spline.tsx
@@ -2,9 +2,12 @@ import * as THREE from 'three';
 import React, { useEffect, useRef } from 'react';
 
 export const Spline = () => {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
     const height = window.innerHeight;
     const width = window.innerWidth;
     const canvasScale = window.devicePixelRatio;
@@ -22,16 +25,23 @@ export const Spline = () => {
 
     renderer.domElement.style.width = '100%';
     renderer.domElement.style.height = '100%';
-    camera.position.x = 22;
+    camera.position.x = 5;
     camera.position.y = 0;
     camera.position.z = 5;
 
+    // Light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+
+    // 3D objects
     const geometry = new THREE.BoxGeometry(2, 2, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
+
+    scene.add(ambientLight);
     scene.add(cube);
 
-    camera.lookAt(cube.position);
+    const target = cube.position.clone();
+    camera.lookAt(target);
 
     const animate = () => {
       requestAnimationFrame(animate);
@@ -42,6 +52,14 @@ export const Spline = () => {
       renderer.render(scene, camera);
     };
     animate();
+  }, []);
+
+  // set the background gradient for the canvas element using CSS
+  useEffect(() => {
+    if (canvasRef.current) {
+      canvasRef.current.style.background =
+        'linear-gradient(to bottom, #1e9088, #00bfff)';
+    }
   }, []);
 
   return <canvas ref={canvasRef} />;
