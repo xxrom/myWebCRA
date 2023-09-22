26db5328a522a3973004db4ddb8809875ebd1c84
ChernyshovNikita <workxrom@gmail.com>
2023-09-22 09:01:57 +0400

    feat: working THREE with MOD3 and GridHelper

--- src/containers/Spline.tsx
+++ src/containers/Spline.tsx
@@ -1,6 +1,8 @@
-import * as THREE from 'three';
 import React, { useEffect, useRef } from 'react';
 import { styled } from '@linaria/react';
+
+const MOD3: any = window.MOD3;
+const THREE: any = window.THREE;
 
 export const Spline = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -19,11 +21,12 @@ export const Spline = () => {
       25, // fov = Field Of View
       width / height, // aspect ratio (dummy value)
       0.1, // near clipping plane
-      100 // far clipping plane
+      1000 // far clipping plane
     );
 
     const renderer = new THREE.WebGLRenderer({
       canvas: canvasRef.current,
+      powerPreference: 'low-power', // default
       //antialias: true,
       alpha: true,
     });
@@ -33,8 +36,17 @@ export const Spline = () => {
     renderer.domElement.style.width = '100%';
     renderer.domElement.style.height = '100%';
     camera.position.x = 52;
-    camera.position.y = 0;
+    camera.position.y = 5;
     camera.position.z = -15;
+
+    // Control camera with mouse
+    //const orbit = new OrbitControls(camera, renderer.domElement);
+    // Axes helpers
+    //const axesHelper = THREE.AxisHelper(15);
+    //scene.add(axesHelper);
+    // Grid Helper
+    const gridHelper = new THREE.GridHelper(15, 1);
+    scene.add(gridHelper);
 
     // Light
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
@@ -91,7 +103,6 @@ export const Spline = () => {
     texture.repeat.set(0.1, 0.1);
 
     // show shape
-    //const geomentry = new THREE.ShapeGeometry(shape);
     const geomentry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
     const material = new THREE.MeshPhysicalMaterial({
       color: 0x049ef4,
@@ -122,17 +133,31 @@ export const Spline = () => {
     ambientLight.position.set(10, 20, 20);
 
     camera.lookAt(target);
+    //orbit.update();
+
+    // MOD
+    // Taper across Z axis
+    const mstack = new MOD3.ModifierStack(MOD3.LibraryThree, cube);
+    const taper = new MOD3.Taper(
+      -1,
+      3,
+      MOD3.Vector3.Z(false),
+      MOD3.Vector3.Z()
+    );
+
+    mstack.addModifier(taper);
+    mstack.apply();
 
     let delta = 0;
     const animate = () => {
-      delta += 0.001;
+      delta += 0.0001;
       texture.offset.set(delta, delta);
 
       requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.02;
-      cube.rotation.y += 0.01;
-      cube.rotation.z += 0.01;
+      cube.rotation.x += 0.002;
+      //cube.rotation.y += 0.01;
+      //cube.rotation.z += 0.01;
 
       renderer.render(scene, camera);
     };
@@ -141,8 +166,6 @@ export const Spline = () => {
 
   return <Canvas ref={canvasRef} />;
 };
-
-//export const Spline = () => renderer.domElement;
 
 const Canvas = styled.canvas`
   background: linear-gradient(-45deg, #ee7752, #008888, #23a6d5, #23d5ab);
