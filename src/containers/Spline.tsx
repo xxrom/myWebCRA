efd1b48ccdf7c0985664fa956e1baa41056f4cd5
ChernyshovNikita <workxrom@gmail.com>
2023-09-25 05:36:43 +0400

    feat: adding new spheres to the scene ...

--- src/containers/Spline.tsx
+++ src/containers/Spline.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { styled } from '@linaria/react';
+import dat from 'dat.gui';
 
 const MOD3: any = window.MOD3;
 const THREE: any = window.THREE;
@@ -18,6 +19,7 @@ const generate_noise2d = (width: number, height: number) => {
 
 export const Spline = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const renderRef = useRef(false);
 
   // Helper
   useEffect(() => {
@@ -25,21 +27,18 @@ export const Spline = () => {
       return;
     }
 
-    const showControls = true;
-    const showHelpers = true;
-  }, []);
-
-  // Spin
-  useEffect(() => {
-    if (!canvasRef.current) {
+    // Open useEffect only once
+    if (renderRef.current) {
       return;
     }
+    renderRef.current = true;
 
+    // Common values
     const height = window.innerHeight;
     const width = window.innerWidth;
     const canvasScale = window.devicePixelRatio;
-
     const scene = new THREE.Scene();
+
     // Add fog for disappiaring the tail in the fog-background
     // Fog starts from 25 to 61 from the camera, really cool effect
     scene.fog = new THREE.Fog(0x142641, 25, 61);
@@ -65,6 +64,107 @@ export const Spline = () => {
     camera.position.x = 52; // 30
     camera.position.y = 5; // 5
     camera.position.z = -12; // -2
+
+    //
+    // GUI PART
+    //
+    const showControls = true;
+    const showHelpers = true;
+
+    const lights = [];
+
+    const circles: any = [];
+
+    const gui = new dat.GUI({
+      autoPlace: false,
+      name: 'Controls',
+    });
+
+    if (!showControls) {
+      gui.hide();
+
+      return;
+    }
+
+    // Add GUI on page
+    gui.domElement.id = 'gui';
+    document.body.appendChild(gui.domElement);
+
+    const lightsFolder = gui.addFolder('Lights');
+
+    const sphereFolder = gui.addFolder('Sphere');
+
+    function addSphere(i: number) {
+      const folder = sphereFolder.addFolder(`Sphere ${i}`);
+      folder.add(circles[i], 'speed', 0, 0.1).name('Speed');
+      folder.add(circles[i], 'radius', 0, 2).name('Radius');
+      folder.addColor(circles[i], 'color').name('Color');
+      folder.add(circles[i].position, 'x', -10, 10).name('X');
+      folder.add(circles[i].position, 'y', -10, 10).name('Y');
+      folder.add(circles[i].position, 'z', -10, 10).name('Z');
+    }
+    const updateCircles = () => {
+      console.log('circles', circles);
+
+      circles.forEach(
+        (
+          {
+            position: { x, y, z },
+            color,
+            radius,
+            mesh: { scale, position, material },
+          }: any,
+          i: number
+        ) => {
+          console.log(circles[i], x);
+          material.color = new THREE.Color(color || 0x00ff00);
+          scale.set(radius, radius, radius);
+          position.set(x, y, z);
+        }
+      );
+    };
+
+    circles.forEach(addSphere);
+
+    const button = {
+      addSphere: function () {
+        const newCircle: any = {
+          speed: 0.01,
+          radius: 0.8,
+          color: 0x00ff00,
+          position: { x: 0, y: 0, z: 0 },
+        };
+
+        const geometry2 = new THREE.SphereGeometry(15, 32, 16);
+        const material2 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const sphere = new THREE.Mesh(geometry2, material2);
+        sphere.castShadow = true;
+        sphere.receiveShadow = true;
+        console.log(sphere);
+
+        circles.push({ ...newCircle, mesh: sphere });
+        const size = circles.length - 1;
+        circles[size].delta = Math.random() * 10;
+
+        scene.add(circles[size].mesh);
+
+        addSphere(size);
+      },
+      addLight: () => {
+        lights.push({
+          color: 0xffffff,
+          intensity: 1,
+          distance: 0,
+          position: { x: 0, y: 0, z: 0 },
+          target: { x: 0, y: 0, z: 0 },
+        });
+      },
+    };
+
+    gui.add(button, 'addSphere').name('Add Sphere');
+    gui.add(button, 'addLight').name('Add Light');
+
+    // MAIN RENDER
 
     // Control camera with mouse
     //const orbit = new OrbitControls(camera, renderer.domElement);
@@ -123,11 +223,7 @@ export const Spline = () => {
 
     // USING THREE DATA TEXTURE To CREATE A RAW DATA TEXTURE
     const texture = new THREE.TextureLoader().load(
-      //process.env.PUBLIC_URL + 'textures/Crystal_001_COLOR.jpg'
-      //process.env.PUBLIC_URL + 'textures/Abstract_010_basecolor.jpg'
-      //process.env.PUBLIC_URL + 'textures/Crystal_001_COLOR.jpg'
       process.env.PUBLIC_URL + 'textures/Crystal_001_DISP.jpg'
-      //'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/golfball.jpg'
     );
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
@@ -150,12 +246,11 @@ export const Spline = () => {
       //flatShading: true,
       //fog: true,
       //wireframe: true,
-      //side: THREE.FrontSide,
+      side: THREE.bothSide,
       map: texture,
     });
 
     const cube = new THREE.Mesh(geomentry, material);
-
     scene.add(cube);
 
     const target = cube.position.clone();
@@ -200,19 +295,24 @@ export const Spline = () => {
     mstack.apply();
 
     let delta = 0;
-    const animate = () => {
+
+    function animate() {
       // Move textures across
       delta += 0.0004;
-      texture.offset.set(delta, delta);
-
-      requestAnimationFrame(animate);
+      texture.offset.set(delta, delta / 2);
+      mstack.apply();
 
       //cube.rotation.x += 0.002;
       //cube.rotation.y += 0.01;
       cube.rotation.z += 0.01;
 
+      // Update circles
+      updateCircles();
+
+      requestAnimationFrame(animate);
       renderer.render(scene, camera);
-    };
+    }
+
     animate();
   }, []);
 
