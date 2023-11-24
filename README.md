23e5519ffb052054e0c6c263b1015623a9dbd37a
Nikita Chernyshov <xxromm3@gmail.com>
2023-11-25 03:37:05 +0400

    feat: added types for redux/saga, added custom fork for running scroll watcher on background !

---
+++ README.md
@@ -0,0 +1,3 @@
+# redux saga good example with TS:
+
+- https://medium.com/simform-engineering/redux-saga-with-typescript-in-react-native-application-d4b9b62d9a62
--- src/store/saga/sagas.ts
+++ src/store/saga/sagas.ts
@@ -1,39 +1,16 @@
 import { put, takeEvery, all, call, spawn } from 'redux-saga/effects';
+import { scroll } from './scroll';
 
 export const delay = (ms = 1000) => new Promise((res) => setTimeout(res, ms));
 
-export function* helloSaga() {
-  console.log('Hello Sagas!');
-  yield delay();
-  console.log('Hello Sagas RUN!');
-  yield put({ type: 'INCREMENT_ASYNC' });
-  console.log('Hello Sagas FINISH!');
-}
+// TODO:
+// 1.[] - add saga that will run onScroll (scrollY) watch event and
+// - will wait until it will be ended by other type event =)
+// 2.[] - add watchers on change in scrollY and will update compnents
+// - information isInView, offsetTop
 
-export function* incrementAsync() {
-  console.log('INCREMENT ASYNC!');
-  yield delay();
-  yield put({ type: 'FINISH_ICREMENT_ASYNC' });
-}
-
-export function* watchIncrementAsync() {
-  yield takeEvery('INCREMENT_ASYNC', incrementAsync);
-}
-
-export function* rerunIncrementAsync() {
-  console.log('RERUN INCREMENT ASYNC!');
-  yield delay();
-  yield put({ type: 'INCREMENT_ASYNC' });
-  throw Error('test error!');
-}
-export function* watchFinishIncrementAsync() {
-  yield takeEvery('FINISH_ICREMENT_ASYNC', rerunIncrementAsync);
-}
-
-// notice how we now only export the rootSaga
-// single entry point to start all Sagas at once
 export default function* rootSaga() {
-  const sagas = [helloSaga, watchIncrementAsync, watchFinishIncrementAsync];
+  const sagas = [scroll];
 
   yield all(
     sagas.map((saga) =>
@@ -49,6 +26,4 @@ export default function* rootSaga() {
       })
     )
   );
-
-  //yield all([helloSaga(), watchIncrementAsync()]);
 }
