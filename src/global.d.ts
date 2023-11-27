40ffb71089301dcaef9376e1a0024118300efdf5
xxrom <chernyshovnm@gmail.com>
2023-11-27 06:55:30 +0400

    feat: fixes tsc errors

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+// global.d.ts
+export {}; // this file needs to be a module
+
+declare global {
+  interface Window {
+    MOD3: any;
+    THREE: any; // You can replace 'any' with a more specific type if you have one.
+  }
+}
+
+type Window = {
+  MOD3: any;
+  THREE: any;
+};
